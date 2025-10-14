dc45307254c7fb0c30b40c8e27b44ee6a9ae7f6f
lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-10-14 14:44:44 +0200

    handle unexpected objects in response data

--- src/contiguity/_response.py
+++ src/contiguity/_response.py
@@ -1,10 +1,11 @@
-from collections.abc import Sequence
+from collections.abc import Mapping, Sequence
 from http import HTTPStatus
 from typing import Generic, TypeVar
 
 import msgspec
 
 T = TypeVar("T", bound=msgspec.Struct | Sequence[msgspec.Struct])
+_type = type
 
 
 class ResponseMetadata(msgspec.Struct):
@@ -35,4 +36,8 @@ def decode_response(content: bytes, /, *, type: type[T]) -> T:
         api_version=raw.api_version,
         object=raw.object,
     )
-    return msgspec.convert({**msgspec.to_builtins(raw.data), "metadata": metadata}, type=type)
+    data = msgspec.to_builtins(raw.data)
+    if not isinstance(data, Mapping):
+        msg = f"expected Mapping instance for 'data' field, got {_type(data)}"
+        raise TypeError(msg)
+    return msgspec.convert({**data, "metadata": metadata}, type=type)
